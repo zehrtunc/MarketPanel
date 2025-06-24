dd192d3521962a15f7ad3b8e9b15743c0ff69021
zehratunc <zehrtunc@gmail.com>
2025-06-24 17:39:47 +0300

    update: ui and logos

--- MarketPanel/Models/Entities/Product.cs
+++ MarketPanel/Models/Entities/Product.cs
@@ -6,10 +6,8 @@ public class Product
 {
     public long Id { get; set; }
 
-    [Required]
     public string Name { get; set; }
 
-    [Required]
     public decimal Price { get; set; } //per unit
 
     public long CategoryId { get; set; }
